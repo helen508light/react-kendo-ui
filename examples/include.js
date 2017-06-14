721e0ee11d58ddcfb28d9f767a19304051a06e34
elle510 <elle0510@gmail.com>
2017-06-14 16:39:41 +0900

    kendo cdn http -> https

--- examples/include.js
+++ examples/include.js
@@ -1,5 +1,5 @@
 document.write('<script type="text/javascript" src="./public/libs/jquery/jquery-3.2.0.min.js"></script>');
 document.write('<script type="text/javascript" src="./public/libs/jquery/jquery-migrate-3.0.0.min.js"></script>');
 document.write('<script type="text/javascript" src="./public/libs/bootstrap-3.3.7/js/bootstrap.min.js"></script>');
-document.write('<script type="text/javascript" src="http://kendo.cdn.telerik.com/2017.2.504/js/kendo.all.min.js"></script>');
+document.write('<script type="text/javascript" src="https://kendo.cdn.telerik.com/2017.2.504/js/kendo.all.min.js"></script>');
 document.write('<script type="text/javascript" src="./public/libs/prettify/prettify.js"></script>');
