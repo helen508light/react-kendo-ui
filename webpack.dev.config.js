4637a21a8749f14ddc6bdd16051e0c36e503a889
elle510 <elle0510@gmail.com>
2017-07-04 18:01:18 +0900

    eslint init

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -32,8 +32,8 @@ module.exports = {
 
     devServer: {
         inline: true,
-        host: '0.0.0.0',
-        port: 4000,
+        host: '127.0.0.1',
+        port: 3000,
         contentBase: __dirname + '/',
     }
 };
